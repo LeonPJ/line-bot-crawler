1268d332a36d004a722fcc29450c17e84efa0ebc
LeonPJ <leon19941214@gmail.com>
2021-02-16 04:07:47 +0800

    instagram crawler web

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 const fb_username = process.env.FB_USERNAME;
 const fb_password = process.env.FB_PASSWORD;
+const ig_username = process.env.IG_USERNAME;
+const ig_password = process.env.IG_PASSWORD;
 
-const webdriver = require('selenium-webdriver'),
-    By = webdriver.By,
-    until = webdriver.until;
-
+const webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const path = require('path');
 const fs = require('fs');
+const request = require('request');
 
 function checkDriver() {
     try {
@@ -63,21 +63,41 @@ async function loginFaceBook() {
 async function loginInstagram() {
     const options = new chrome.Options();
     options.setUserPreferences({ 'profile.default_content_setting_values.notifications': 1 });//close FaceBook notifications
-    let driver = await new webdriver.Builder().forBrowser('chrome').withCapabilities(options).build();
-    const webInstagram = 'https://www.instagram.com/accounts/login/';
+    let driver = await new webdriver.Builder().forBrowser('chrome').withCapabilities(options,
+        { acceptSslCerts: true, acceptInsecureCerts: true }).build();//這是為了解決跨網域問題
+    await driver.manage().window().setRect({ width: 1200, height: 800, x: 0, y: 0 });//設定螢幕大小
+    const webInstagram = 'https://www.instagram.com/';
     await driver.get(webInstagram);
-    const fb_email_ele = await driver.wait(until.elementLocated(By.xpath(`//*[@id="loginForm"]/div/div[1]/div/label/input`)));//username
-    fb_email_ele.sendKeys(fb_username);
-    const fb_password_ele = await driver.wait(until.elementLocated(By.xpath(`//*[@id="loginForm"]/div/div[2]/div/label/input`)));//password
-    fb_password_ele.sendKeys(fb_password);
-    const fb_login_ele = await driver.wait(until.elementLocated(By.xpath(`//*[@id="loginForm"]/div/div[3]`)));//login
-    fb_login_ele.click();
+    const ig_email_ele = await driver.wait(webdriver.until.elementLocated(webdriver.By.xpath(`//*[@id="loginForm"]/div/div[1]/div/label/input`)));//username
+    ig_email_ele.sendKeys(ig_username);
+    const ig_password_ele = await driver.wait(webdriver.until.elementLocated(webdriver.By.xpath(`//*[@id="loginForm"]/div/div[2]/div/label/input`)));//password
+    ig_password_ele.sendKeys(ig_password);
+    const ig_login_ele = await driver.wait(webdriver.until.elementLocated(webdriver.By.xpath(`//*[@id="loginForm"]/div/div[3]`)));//login
+    ig_login_ele.click();
+
+    await driver.wait(webdriver.until.elementLocated(webdriver.By.xpath(`//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div`)));// 確認是否登入成功
+
+    const followerPage = 'https://www.instagram.com/cindiatsai/';
+    await driver.get(followerPage);
+    await driver.sleep(3000);
+    /*let fb_trace = 0;
+    const fb_trace_eles = await driver.wait(until.elementsLocated(By.xpath(`//*[contains(@class,"d2edcug0 hpfvmrgz qv66sw1b c1et5uql rrkovp55 jq4qci2q a3bd9o3v knj5qynh oo9gr5id")]`)));
+    for (const fb_trace_ele of fb_trace_eles) {
+        const fb_text = await fb_trace_ele.getText();
+        if (fb_text.includes('人')) {
+            fb_trace = fb_text.replace('人', '');
+            break;
+        }
+    }
+    console.log(`Erika 按讚人數: ${fb_trace} 人`);//按讚人數
+    driver.quit();*/
 }
 
 async function openCrawlerWeb() {
     if (!checkDriver())
         return
-    loginFaceBook();
+    //loginFaceBook();
+    loginInstagram();
 }
 
 openCrawlerWeb();
