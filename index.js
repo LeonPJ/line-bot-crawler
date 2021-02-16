a5e4173c4c07d5fe3ebafe907f223f79253a3356
LeonPJ <leon19941214@gmail.com>
2021-02-17 02:17:23 +0800

    instagram crawler source code

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const fb_password = process.env.FB_PASSWORD;
 const ig_username = process.env.IG_USERNAME;
 const ig_password = process.env.IG_PASSWORD;
 
-const webdriver = require('selenium-webdriver');
+const webdriver = require('selenium-webdriver'), // 加入虛擬網頁套件
+    By = webdriver.By,//你想要透過什麼方式來抓取元件，通常使用xpath、css
+    until = webdriver.until;//直到抓到元件才進入下一步(可設定等待時間)
 const chrome = require('selenium-webdriver/chrome');
 const path = require('path');
 const fs = require('fs');
-const request = require('request');
 
 function checkDriver() {
     try {
@@ -68,29 +69,22 @@ async function loginInstagram() {
     await driver.manage().window().setRect({ width: 1200, height: 800, x: 0, y: 0 });//設定螢幕大小
     const webInstagram = 'https://www.instagram.com/';
     await driver.get(webInstagram);
-    const ig_email_ele = await driver.wait(webdriver.until.elementLocated(webdriver.By.xpath(`//*[@id="loginForm"]/div/div[1]/div/label/input`)));//username
+    const ig_email_ele = await driver.wait(until.elementLocated(By.xpath(`//*[@id="loginForm"]/div/div[1]/div/label/input`)));//username
     ig_email_ele.sendKeys(ig_username);
-    const ig_password_ele = await driver.wait(webdriver.until.elementLocated(webdriver.By.xpath(`//*[@id="loginForm"]/div/div[2]/div/label/input`)));//password
+    const ig_password_ele = await driver.wait(until.elementLocated(By.xpath(`//*[@id="loginForm"]/div/div[2]/div/label/input`)));//password
     ig_password_ele.sendKeys(ig_password);
-    const ig_login_ele = await driver.wait(webdriver.until.elementLocated(webdriver.By.xpath(`//*[@id="loginForm"]/div/div[3]`)));//login
+    const ig_login_ele = await driver.wait(until.elementLocated(By.xpath(`//*[@id="loginForm"]/div/div[3]`)));//login
     ig_login_ele.click();
 
-    await driver.wait(webdriver.until.elementLocated(webdriver.By.xpath(`//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div`)));// 確認是否登入成功
+    await driver.wait(until.elementLocated(By.xpath(`//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div`)));// 確認是否登入成功
 
     const followerPage = 'https://www.instagram.com/cindiatsai/';
     await driver.get(followerPage);
     await driver.sleep(3000);
-    /*let fb_trace = 0;
-    const fb_trace_eles = await driver.wait(until.elementsLocated(By.xpath(`//*[contains(@class,"d2edcug0 hpfvmrgz qv66sw1b c1et5uql rrkovp55 jq4qci2q a3bd9o3v knj5qynh oo9gr5id")]`)));
-    for (const fb_trace_ele of fb_trace_eles) {
-        const fb_text = await fb_trace_ele.getText();
-        if (fb_text.includes('人')) {
-            fb_trace = fb_text.replace('人', '');
-            break;
-        }
-    }
-    console.log(`Erika 按讚人數: ${fb_trace} 人`);//按讚人數
-    driver.quit();*/
+    const pageSource = await driver.wait(until.elementLocated(By.css('body')), 5000).getAttribute('innerHTML');
+    console.log('pageSource: ', pageSource);
+
+    //console.log("hi");
 }
 
 async function openCrawlerWeb() {
